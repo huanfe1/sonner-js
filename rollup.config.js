9542f2994bc1e308f531e0bb308d25086ed79774
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2025-03-10 21:52:06 +0800

    chore: update package

--- rollup.config.js
+++ rollup.config.js
@@ -5,30 +5,21 @@ import postcss from 'rollup-plugin-postcss';
 export default defineConfig([
     {
         input: 'src/index.ts',
-        output: [
-            {
-                file: './dist/esm/index.js',
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
+        output: {
+            file: './dist/esm/index.js',
+            format: 'esm',
+            sourcemap: true,
+        },
         plugins: [typescript({ tsconfig: './tsconfig.json' }), postcss()],
     },
     {
         input: 'src/index.ts',
-        output: [
-            {
-                format: 'umd',
-                name: 'toast',
-                file: './dist/umd/index.js',
-                sourcemap: true,
-            },
-            {
-                format: 'cjs',
-                file: './dist/cjs/index.js',
-                sourcemap: true,
-            },
-        ],
+        output: {
+            format: 'umd',
+            name: 'toast',
+            file: './dist/umd/index.js',
+            sourcemap: true,
+        },
         plugins: [typescript({ tsconfig: false, noCheck: true }), postcss()],
     },
 ]);
