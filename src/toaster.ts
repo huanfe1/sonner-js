bdfde53be95cf5783b8414e5176a72ec04029018
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2025-07-03 15:40:56 +0800

    fix: wrong toast enter animation

--- src/toaster.ts
+++ src/toaster.ts
@@ -81,7 +81,6 @@ export function assignOffset(container: HTMLElement) {
     if (toasts.length === 0) return;
 
     const frontToast = toasts[0];
-    frontToast.setAttribute('data-mounted', 'true');
     if (!getPropertyValue(frontToast, 'init-height')) {
         frontToast.style.setProperty('--init-height', `${frontToast.offsetHeight}px`);
     }
@@ -93,11 +92,7 @@ export function assignOffset(container: HTMLElement) {
         toast.style.setProperty('--offset', `${offset}px`);
         toast.style.setProperty('--index', index.toString());
 
-        if (index + 1 > visibleToasts) {
-            toast.style.setProperty('--opacity', '0');
-        } else {
-            toast.style.setProperty('--opacity', '1');
-        }
+        toast.setAttribute('data-state', index + 1 > visibleToasts ? 'invisible' : 'mounted');
     });
 
     container.style.setProperty('--front-height', `${toasts[0]?.offsetHeight}px`);
